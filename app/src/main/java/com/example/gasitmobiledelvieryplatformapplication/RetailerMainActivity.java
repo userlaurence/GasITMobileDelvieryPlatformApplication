8af4a24192d0db4a2f15d02c60e9c74ce02a0b2d
Philiks <janopol.felixjr@gmail.com>
2021-12-03 20:40:22 +0800

    Retailer Main Activity (unimplemented yet).

---
+++ app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/RetailerMainActivity.java
@@ -0,0 +1,41 @@
+package com.example.gasitmobiledelvieryplatformapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.gasitmobiledelvieryplatformapplication.fragment.LocationFragment;
+import com.example.gasitmobiledelvieryplatformapplication.fragment.OrderFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class RetailerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main);
+
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+//                new HomeFragment()).commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment;
+
+            int id = item.getItemId();
+            //TODO: Add retailer navigation fragments.
+//            if (id == R.id.navHome)             selectedFragment = new HomeFragment();
+//            else if (id == R.id.navOrder)       selectedFragment = new OrderNotificationFragment();
+//            else if (id == R.id.navWarehouse)   selectedFragment = new WarehouseFragment();
+//            else if (id == R.id.navSales)       selectedFragment = new SalesFragment();
+//            else return false;
+
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+//                    selectedFragment).commit();
+
+            return true;
+        });
+    }
+
+}

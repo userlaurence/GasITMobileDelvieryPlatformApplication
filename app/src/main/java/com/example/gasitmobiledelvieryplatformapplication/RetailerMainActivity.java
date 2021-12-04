8226b6edcb4eec8b503f1973a8bd901bcb032d67
Philiks <janopol.felixjr@gmail.com>
2021-12-04 12:05:38 +0800

    Directs to HomeFragment.java as default fragment in navigation.

--- app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/RetailerMainActivity.java
+++ app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/RetailerMainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.gasitmobiledelvieryplatformapplication.fragment.HomeFragment;
 import com.example.gasitmobiledelvieryplatformapplication.fragment.LocationFragment;
 import com.example.gasitmobiledelvieryplatformapplication.fragment.OrderFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,8 +17,8 @@ public class RetailerMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
 
-//        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
-//                new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                new HomeFragment()).commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -25,14 +26,14 @@ public class RetailerMainActivity extends AppCompatActivity {
 
             int id = item.getItemId();
             //TODO: Add retailer navigation fragments.
-//            if (id == R.id.navHome)             selectedFragment = new HomeFragment();
+            if (id == R.id.navHome)             selectedFragment = new HomeFragment();
 //            else if (id == R.id.navOrder)       selectedFragment = new OrderNotificationFragment();
 //            else if (id == R.id.navWarehouse)   selectedFragment = new WarehouseFragment();
 //            else if (id == R.id.navSales)       selectedFragment = new SalesFragment();
-//            else return false;
+            else return false;
 
-//            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
-//                    selectedFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                    selectedFragment).commit();
 
             return true;
         });

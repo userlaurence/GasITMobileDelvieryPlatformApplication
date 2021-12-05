23cc0b3460a57718e0851f1855eb364296938de0
Philiks <janopol.felixjr@gmail.com>
2021-12-05 11:58:18 +0800

    Utility functions for detecting misinputs on Fields.

---
+++ app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/util/FieldUtil.java
@@ -0,0 +1,64 @@
+package com.example.gasitmobiledelvieryplatformapplication.util;
+
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import es.dmoral.toasty.Toasty;
+
+public class FieldUtil {
+    public static int MIN_PASSWORD_LENGTH = 8;
+
+    public static boolean isEmptyEditText(String field, EditText editText) {
+        if (!editText.getText().toString().trim().isEmpty())
+            return false;
+
+        editText.setError(field + " should not be empty.");
+        editText.requestFocus();
+        return true;
+    }
+
+    public static boolean haveNotSelectedRadioButton(RadioGroup radioGroup) {
+        if (radioGroup.getCheckedRadioButtonId() != -1)
+            return false;
+
+        RadioButton radioButton = (RadioButton) radioGroup.getChildAt(0);
+        radioButton.requestFocus();
+        radioButton.setError("Please select one of the options.");
+        return true;
+    }
+
+    public static boolean isEmailIncorrect(EditText editText) {
+        if (Patterns.EMAIL_ADDRESS.matcher(
+                editText.getText().toString().trim())
+                .matches())
+            return false;
+
+        editText.setError("Please provide valid email.");
+        editText.requestFocus();
+        return true;
+    }
+
+    public static boolean isIncorrectPasswordLength(String passwordField, EditText editText) {
+        if (isEmptyEditText(passwordField, editText))
+            return true;
+
+        if (editText.getText().toString().trim().length() >= MIN_PASSWORD_LENGTH)
+            return false;
+
+        editText.setError("Minimum password length should be 8 characters.");
+        editText.requestFocus();
+        return true;
+    }
+
+    public static boolean areDifferentPasswords(EditText aEditText, EditText bEditText) {
+        if (aEditText.getText().toString().trim()
+                .equals(bEditText.getText().toString().trim()))
+            return false;
+
+        bEditText.setError("Confirm Password is different from Password.");
+        bEditText.requestFocus();
+        return true;
+    }
+}

8e49a1d15ba9225953a16347026c5a1d694587eb
Philiks <janopol.felixjr@gmail.com>
2021-12-08 18:38:17 +0800

    Added notification counts.

--- app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/RetailerMainActivity.java
+++ app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/RetailerMainActivity.java
@@ -1,40 +1,93 @@
 package com.example.gasitmobiledelvieryplatformapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.gasitmobiledelvieryplatformapplication.fragment.HomeFragment;
+import com.example.gasitmobiledelvieryplatformapplication.fragment.OrderStatusFragment;
+import com.example.gasitmobiledelvieryplatformapplication.fragment.RetailerHomeFragment;
+import com.example.gasitmobiledelvieryplatformapplication.fragment.SalesFragment;
+import com.example.gasitmobiledelvieryplatformapplication.model.Order;
+import com.example.gasitmobiledelvieryplatformapplication.model.ListItemRequestCallback;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 public class RetailerMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_main);
+        setContentView(R.layout.activity_retailer_main);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
-                new HomeFragment()).commit();
+                new RetailerHomeFragment()).commit();
+
+        countNotification(Order.STATUS_PENDING, findViewById(R.id.pendingNotificationTextView));
+        countNotification(Order.STATUS_SHIPPING, findViewById(R.id.shippingNotificationTextView));
+        countNotification(Order.STATUS_DELIVERED, findViewById(R.id.deliveredNotificationTextView));
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment selectedFragment;
 
+            String title;
             int id = item.getItemId();
+            if (id == R.id.navHome) {
+                title = getString(R.string.nav_home);
+                selectedFragment = new RetailerHomeFragment();
+            } else if (id == R.id.navPending) {
+                title = getString(R.string.nav_order_pending);
+                selectedFragment = new OrderStatusFragment(Order.STATUS_PENDING);
+            } else if (id == R.id.navShipping) {
+                title = getString(R.string.nav_order_shipping);
+                selectedFragment = new OrderStatusFragment(Order.STATUS_SHIPPING);
+            } else if (id == R.id.navDelivered) {
+                title = getString(R.string.nav_order_delivered);
+                selectedFragment = new OrderStatusFragment(Order.STATUS_DELIVERED);
+            }
             //TODO: Add retailer navigation fragments.
-            if (id == R.id.navHome)             selectedFragment = new HomeFragment();
-//            else if (id == R.id.navOrder)       selectedFragment = new OrderNotificationFragment();
-//            else if (id == R.id.navWarehouse)   selectedFragment = new WarehouseFragment();
-//            else if (id == R.id.navSales)       selectedFragment = new SalesFragment();
+//            else if (id == R.id.navWarehouse) {
+//                title = "Warehouse";
+//                selectedFragment = new WarehouseFragment();
+//            }
+            else if (id == R.id.navSales) {
+                title = "Sales";
+                selectedFragment = new SalesFragment();
+            }
             else return false;
 
+            ((TextView) findViewById(R.id.fragmentTitleTextView)).setText(title);
             getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
                     selectedFragment).commit();
 
             return true;
         });
+
     }
 
+    public void recountNotifications() {
+        countNotification(Order.STATUS_PENDING, findViewById(R.id.pendingNotificationTextView));
+        countNotification(Order.STATUS_SHIPPING, findViewById(R.id.shippingNotificationTextView));
+        countNotification(Order.STATUS_DELIVERED, findViewById(R.id.deliveredNotificationTextView));
+    }
+
+    private void countNotification(String status, TextView notificationTextView) {
+        new Order(status).readAll(new ListItemRequestCallback<Order>() {
+            @Override
+            public void onSuccess(List<Order> orderList) {
+                int size = orderList == null ? 0 : orderList.size();
+                notificationTextView.setVisibility(size == 0 ? View.GONE : View.VISIBLE);
+                notificationTextView.setText(String.valueOf(size));
+            }
+
+            @Override
+            public void onFailure(String error) {
+                // Do nothing.
+            }
+        });
+    }
 }

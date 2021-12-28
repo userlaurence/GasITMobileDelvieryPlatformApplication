70a836df7e3f39290dbc939fed1fdcf9982ac3df
MichaelCerro <82153261+miivkyo08@users.noreply.github.com>
2021-12-28 13:03:05 +0800

    add ons new changes

--- app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/profile_navigation.java
+++ app/src/main/java/com/example/gasitmobiledelvieryplatformapplication/profile_navigation.java
@@ -38,7 +38,7 @@ private ActivityProfileNavigationBinding binding;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.the_username, R.id.the_usernumber, R.id.the_useraddress)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_profile_navigation);
